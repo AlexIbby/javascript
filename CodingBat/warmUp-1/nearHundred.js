7ca5d64108fbd5a4ee718737ef737f32bdebe89f
AlexIbby <alex.a.ibrahim@gmail.com>
2022-08-22 22:26:31 -0400

    nearHundred completed

---
+++ CodingBat/warmUp-1/nearHundred.js
@@ -0,0 +1,19 @@
+function nearHundred(n){
+  let hundred_low = 89
+  let hundred_high = 111
+  
+  2hundred_low = 189
+  2hundred_high = 211
+
+  if (n > 89 && n < 111){
+    return true 
+    
+  }else if (n > 189 && n < 211) {
+    return true 
+   
+  }else{
+    return false 
+  }
+   
+ 
+}
