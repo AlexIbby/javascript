bfd148d91bb7be6537630c676a34f4a2bc594995
AlexIbby <alex.a.ibrahim@gmail.com>
2022-09-15 08:18:24 -0400

    added exercise startOz

---
+++ CodingBat/warmUp-1/startOz.js
@@ -0,0 +1,12 @@
+function startOz(str){
+  if ( (str[0] === "o") && (str[1] != "z")){
+    return "o"
+  }else if ( (str[0] === "o") && (str[1] === "z")){
+    return "oz"
+  
+  }else if ( (str[0] != "o") && (str[1] === "z"){
+    return "z"      
+        }else{
+    return ""
+  }
+}
