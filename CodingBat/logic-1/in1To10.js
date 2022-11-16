b459caa77b18229c9483cdc9b326d167e220c17c
AlexIbby <alex.a.ibrahim@gmail.com>
2022-11-16 10:29:30 -0500

    completed in1to10 exercise

---
+++ CodingBat/logic-1/in1To10.js
@@ -0,0 +1,16 @@
+function in1To10(n, outsideMode){
+  
+  if ((n >= 1) && (n <= 10) && (outsideMode === false)){
+  
+    return true
+    
+  }else if (((n <= 1) || (n >= 10)) && (outsideMode === true)){
+  
+    return true
+    
+  }else{
+  
+    return false
+  }
+  
+}
