4e5d4a24f0b9cbd7caa9ce2d272d4b06a59da4d5
AlexIbby <alex.a.ibrahim@gmail.com>
2022-11-20 10:10:35 -0500

    completed answerCell exercise

---
+++ CodingBat/logic-1/answerCell.js
@@ -0,0 +1,20 @@
+function answerCell(isMorning, isMom, isAsleep){
+  
+  if (isAsleep === true){
+  
+    return false 
+  }
+  
+  if ((isMorning === true) && (isMom === false)){
+  
+    return false
+  }
+  
+  if (isMorning === true){
+  
+    false
+  }
+  
+  return true
+  
+}
