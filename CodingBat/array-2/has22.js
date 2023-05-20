8855afd98ca4e82a03f192e6e19b0a7f8cbef49a
AlexIbby <alex.a.ibrahim@gmail.com>
2023-05-20 19:07:28 -0400

    completed has22 exercise

---
+++ CodingBat/array-2/has22.js
@@ -0,0 +1,15 @@
+function has22(nums){
+  
+  for (let i = 0; i < nums.length; i++){
+  
+    if (nums[i] === 2 && nums[i+1] === 2 ){
+      
+      return true
+  }
+  
+  }
+  
+      return false 
+      
+  
+};
