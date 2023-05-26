4feb720c65f214b4fc4f0a12cae86cd4b6e15e02
AlexIbby <alex.a.ibrahim@gmail.com>
2023-05-26 19:10:09 -0400

    completed fizzArray2 exercise

---
+++ CodingBat/array-2/fizzArray2.js
@@ -0,0 +1,22 @@
+function fizzArray2(n){
+  
+  let newArray = []
+  let i = 0
+  
+  if (n === 0){
+  
+    newArray
+  }
+  
+  while (n != 0){
+    
+    newArray.push(i.toString())
+    i++
+    n -= 1
+  
+    
+  }
+  
+  return newArray
+  
+}
