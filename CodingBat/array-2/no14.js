5ac160d311354c6785a03e7aee8515fad4599563
AlexIbby <alex.a.ibrahim@gmail.com>
2023-05-27 19:10:42 -0400

    completed no14 exercise

---
+++ CodingBat/array-2/no14.js
@@ -0,0 +1,13 @@
+function no14(nums){
+  
+  let check4 = nums.includes(4)
+  let check1 = nums.includes(1)
+  
+  if (!check4 || !check1){
+  
+    return true
+  }else{
+    return false
+  }
+  
+}
