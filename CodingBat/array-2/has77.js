14e601d1e7f01e4f0694a0fefaea59ffb9012c63
AlexIbby <alex.a.ibrahim@gmail.com>
2023-05-30 19:12:04 -0400

    completed has 77 exercise

---
+++ CodingBat/array-2/has77.js
@@ -0,0 +1,20 @@
+function has77(nums){
+  
+  nums = nums.join("")
+  
+  if (nums.includes("77"){
+      
+        return true 
+      }
+  
+  const re = /7.7/
+  
+  if (nums.match(re) != null){
+  
+    return true
+  
+  }
+  
+  return false
+  
+}
